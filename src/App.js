7bcdfd6e9de59e92779a568349a5daf7f8f9eb34
Michael White <hrt666.cs@gmail.com>
2023-08-16 18:51:20 +0900

    updates in App and NotesListPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage.js'
 
 function App() {
+  // return (
+  //   <Router>
+  //     <div className="App">
+  //       <Header />
+  //       <Route path="/" exact component={NotesListPage} />
+  //     </div>
+  //   </Router>
+  // )
+
+
   return (
     <div className="App">
       <Header />
